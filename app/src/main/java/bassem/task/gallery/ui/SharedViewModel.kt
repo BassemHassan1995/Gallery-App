cc255c1e1f562efb736b298e8685e997baa04a6a
Bassem Hassan <bassem.hassan1995@gmail.com>
2022-09-27 02:19:15 +0200

    add videos media store

--- app/src/main/java/bassem/task/gallery/ui/SharedViewModel.kt
+++ app/src/main/java/bassem/task/gallery/ui/SharedViewModel.kt
@@ -7,23 +7,25 @@ import bassem.task.gallery.data.model.MediaItem
 import bassem.task.gallery.ui.base.BaseViewModel
 import bassem.task.gallery.ui.base.Event
 import bassem.task.gallery.utils.getAlbumName
-import bassem.task.gallery.utils.getMediaItem
+import bassem.task.gallery.utils.getImage
+import bassem.task.gallery.utils.getVideo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-private const val TAG = "SharedViewModel"
+private const val ALL_VIDEOS_ALBUM = "All Videos"
+private const val ALL_IMAGES_ALBUM = "All Images"
 
 class SharedViewModel(application: Application) : BaseViewModel(application) {
-
-    private val _galleryAlbums: MutableStateFlow<List<Album>> = MutableStateFlow(emptyList())
-    val galleryAlbums: StateFlow<List<Album>> = _galleryAlbums
 
     private val _albumsMap: MutableStateFlow<Map<String, List<MediaItem>>> =
         MutableStateFlow(emptyMap())
 
-    private val _selectedAlbumImages: MutableStateFlow<List<MediaItem>> =
+    private val _galleryAlbums: MutableStateFlow<List<Album>> = MutableStateFlow(emptyList())
+    val galleryAlbums: StateFlow<List<Album>> = _galleryAlbums
+
+    private val _selectedAlbumMedia: MutableStateFlow<List<MediaItem>> =
         MutableStateFlow(emptyList())
-    val selectedAlbumImages: StateFlow<List<MediaItem>> = _selectedAlbumImages
+    val selectedAlbumImages: StateFlow<List<MediaItem>> = _selectedAlbumMedia
 
     init {
         sendEvent(Event.CheckPermissionEvent)
@@ -31,23 +33,30 @@ class SharedViewModel(application: Application) : BaseViewModel(application) {
 
     private fun getGallery() {
         launchCoroutine {
-            _albumsMap.value = getGalleryInfo()
+            _albumsMap.value = getGalleryMedia()
             _galleryAlbums.value = _albumsMap.value.entries.map {
                 Album(
                     name = it.key,
                     count = it.value.size,
                     thumbnail = it.value.firstOrNull()?.contentUri
                 )
-            }
+            }.sortedBy { it.name }
         }
     }
 
-    private fun getAlbumImages(albumName: String): List<MediaItem> =
-        _albumsMap.value.getOrElse(albumName) { emptyList() }
+    fun getAlbumMediaItems(album: Album) {
+        launchCoroutine {
+            val albumMedia = _albumsMap.value.getOrElse(album.name) { emptyList() }
+            _selectedAlbumMedia.emit(albumMedia)
+        }
+    }
 
-    private fun getGalleryInfo(): Map<String, List<MediaItem>> =
+    private fun getGalleryMedia(): Map<String, List<MediaItem>> =
+        getGalleryImages().plus(getGalleryVideos())
+
+    private fun getGalleryImages(): Map<String, List<MediaItem>> =
         mutableMapOf<String, List<MediaItem>>().apply {
-            val projection = arrayOf(
+            val imagesProjection = arrayOf(
                 MediaStore.Images.Media._ID,                        //Image Id
                 MediaStore.Images.Media.DISPLAY_NAME,               //Image name
                 MediaStore.Images.Media.BUCKET_DISPLAY_NAME,        //Album name
@@ -55,26 +64,49 @@ class SharedViewModel(application: Application) : BaseViewModel(application) {
 
             getApplication<Application>().contentResolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                projection,
+                imagesProjection,
                 null,
                 null,
-                null
+                "${MediaStore.Images.Media.DATE_ADDED} DESC"
+
             )?.use { cursor ->
                 while (cursor.moveToNext()) {
 
-                    val mediaItem = cursor.getMediaItem()
+                    val mediaItem = cursor.getImage()
                     val albumName = cursor.getAlbumName()
 
                     set(albumName, get(albumName).orEmpty().plus(mediaItem))
+                    set(ALL_IMAGES_ALBUM, get(ALL_IMAGES_ALBUM).orEmpty().plus(mediaItem))
                 }
             }
         }
 
-    fun getAlbumMediaItems(album: Album) {
-        launchCoroutine {
-            _selectedAlbumImages.emit(getAlbumImages(album.name))
+    private fun getGalleryVideos(): Map<String, List<MediaItem>> =
+        mutableMapOf<String, List<MediaItem>>().apply {
+            val videoProjection = arrayOf(
+                MediaStore.Video.Media._ID,                        //Video Id
+                MediaStore.Video.Media.DISPLAY_NAME,               //Video name
+                MediaStore.Video.Media.BUCKET_DISPLAY_NAME,        //Album name
+            )
+
+            getApplication<Application>().contentResolver.query(
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                videoProjection,
+                null,
+                null,
+                "${MediaStore.Video.Media.DATE_ADDED} DESC"
+
+            )?.use { cursor ->
+                while (cursor.moveToNext()) {
+
+                    val mediaItem = cursor.getVideo()
+                    val albumName = cursor.getAlbumName()
+
+                    set(albumName, get(albumName).orEmpty().plus(mediaItem))
+                    set(ALL_VIDEOS_ALBUM, get(ALL_VIDEOS_ALBUM).orEmpty().plus(mediaItem))
+                }
+            }
         }
-    }
 
     override fun handleOnPermissionGranted() {
         getGallery()
--- app/src/main/java/bassem/task/gallery/utils/Exensions.kt
+++ app/src/main/java/bassem/task/gallery/utils/Exensions.kt
@@ -30,7 +30,7 @@ fun Fragment.showToast(stringId: Int) {
 }
 
 
-fun Cursor.getMediaItem(): MediaItem {
+fun Cursor.getImage(): MediaItem {
     val idColumn = getColumnIndexOrThrow(MediaStore.Images.Media._ID)
     val nameColumn = getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
 
@@ -39,6 +39,19 @@ fun Cursor.getMediaItem(): MediaItem {
 
     val contentUri =
         ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+    return MediaItem(id, name, contentUri)
+}
+
+fun Cursor.getVideo(): MediaItem {
+    val idColumn = getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+    val nameColumn = getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
+
+    val id = getLong(idColumn)
+    val name = getString(nameColumn)
+
+    val contentUri =
+        ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id)
 
     return MediaItem(id, name, contentUri)
 }
